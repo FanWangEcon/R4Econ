d94ce7936a5cc37a1858a8c719c0cef46a050abb
Fan Wang <wangfanbsg75@live.com>
2019-03-23 13:27:38 -0500

    Bullet points one more tab

--- summarize/summpreamble.R
+++ summarize/summpreamble.R
@@ -6,6 +6,7 @@ source("ByGroupCountUniqueIndi.R")
 setwd("C:/Users/fan/R4Econ/summarize/summ/")
 source("ByGroupsSummWide.R")
 source("ByGroupSumm.R")
+source("ByGroupSummOne.R")
 source("SummPercentiles.R")
 
 setwd("C:/Users/fan/R4Econ/summarize/tabulate/")
