5514d34b2f991845a89fe5761cbef870d52a1e89
Fan Wang <wangfanbsg75@live.com>
2019-04-07 19:19:50 -0500

    tricks generate

--- support/dplyrtricks/generate.R
+++ support/dplyrtricks/generate.R
@@ -1,2 +1,6 @@
 # Generate logs of all variables satisfing some condition, new log variables end with _log suffix
 df <- df %>% mutate_at(vars(matches('prot|cal'), -contains('p.A.')), funs(log = log(.)))
+
+# Subtract from all variables some value, add suffix to name, then rename
+mutate_at(vars(starts_with('hgtp.')), funs(hgtdiff = . - !!sym(var.hgt))) %>% 
+  rename_at(vars(ends_with("hgtdiff")), funs(gsub("hgtp.", "hgtpd.", gsub("_hgtdiff", "", .))))
