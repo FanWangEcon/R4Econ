0c0843cac88eeada6a7f7c03295ce45b81b0e411
Fan Wang <wangfanbsg75@live.com>
2019-04-18 21:33:10 -0500

    conda folder jupyterlab, cumsum

--- support/controls/condainstalls.R
+++ support/controls/condainstalls.R
@@ -2,19 +2,38 @@
 # Use minimal numbers of packages, install packages from conda-forge
 # From Anaconda Prompt right click run as Administrator
 
-# Programming
+#############################
+# Jupyter Lab Related
+#############################
+
+# Set up to install jupyterlab addon packages
+conda upgrade -c conda-forge jupyterlab
+conda install -c conda-forge nodejs
+
+# Allow for Directory View in JupyterLab
+jupyter labextension install jupyterlab_filetree
+
+
+#############################
+# R Related
+#############################
+
+# R kernel
 conda install -c r r-irkernel
 
-# Main
+# Tidyverse
 # https://anaconda.org/conda-forge/r-tidyverse
 conda install -c r r-tidyverse
 
-# Regression Packges
+# Linear Regression Package
 # https://anaconda.org/conda-forge/r-aer
 conda install -c conda-forge r-aer
+
+# Nonlinear Regression Package
 # https://anaconda.org/conda-forge/r-minpack.lm
 conda install -c conda-forge r-minpack.lm
 
-
 # Allowing for Invoking Matlab Files from Inside R
 conda install -c conda-forge r-matlab
+
+#
--- support/dplyrtricks/generate.R
+++ support/dplyrtricks/generate.R
@@ -2,5 +2,16 @@
 df <- df %>% mutate_at(vars(matches('prot|cal'), -contains('p.A.')), funs(log = log(.)))
 
 # Subtract from all variables some value, add suffix to name, then rename
-mutate_at(vars(starts_with('hgtp.')), funs(hgtdiff = . - !!sym(var.hgt))) %>% 
+mutate_at(vars(starts_with('hgtp.')), funs(hgtdiff = . - !!sym(var.hgt))) %>%
   rename_at(vars(ends_with("hgtdiff")), funs(gsub("hgtp.", "hgtpd.", gsub("_hgtdiff", "", .))))
+
+# Cumulative Sum Replacing Zeros
+options(warn=0)
+var.id <- 'ID'
+var.mth <- 'svymthRound'
+var.input <- 'cal.imputed'
+var.input.cumu <- 'cal.imputed.cumu'
+df.reg <- df.reg %>%
+        arrange(!!sym(var.id), !!sym(var.mth)) %>%
+        group_by(!!sym(var.id)) %>%
+        mutate(!!var.input.cumu := cumsum(replace_na(!!sym(var.input), 0)))
