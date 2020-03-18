c0c1c1d398c7ad2fd03c63be4fb8641f0c364622
Fan Wang <wangfanbsg75@live.com>
2020-03-18 15:31:56 -0500

    variable naming

--- support/tibble/fs_tib_basics.R
+++ support/tibble/fs_tib_basics.R
@@ -9,7 +9,7 @@
 #' always_allow_html: yes
 #' ---
 #' 
-#' Go back to [fan](http://fanwangecon.github.io/CodeDynaAsset/)'s [R4Econ](https://fanwangecon.github.io/R4Econ/) Repository or [Intro Stats with R](https://fanwangecon.github.io/Stat4Econ/) Repository.
+#' Go back to [fan](http://fanwangecon.github.io/CodeDynaAsset/)'s [REconTools](https://fanwangecon.github.io/REconTools/) Package, [R4Econ](https://fanwangecon.github.io/R4Econ/) Repository, or [Intro Stats with R](https://fanwangecon.github.io/Stat4Econ/) Repository.
 #' 
 #' 
 ## ----GlobalOptions, echo = T, results = 'hide', message=F, warning=F----------
