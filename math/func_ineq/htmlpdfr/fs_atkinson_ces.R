14c9ab2a246793ec4deecb9dbed16ed92e1dd6aa
Fan Wang <wangfanbsg75@live.com>
2020-06-12 09:51:47 -0500

    Atkinson Inequality Statistics

--- math/func_ineq/htmlpdfr/fs_atkinson_ces.R
+++ math/func_ineq/htmlpdfr/fs_atkinson_ces.R
@@ -1,8 +1,80 @@
-## ----global_options, include = FALSE-------------------------------------------------------------------------------------------------------------------
+## ----global_options, include = FALSE------------------------------------------------------------------------------------------------
 try(source("../../.Rprofile"))
 
 
-## ------------------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------------------------------------------------
+# Formula
+ffi_atkinson_ineq <- function(ar_data, fl_rho) {
+  ar_data_demean <- ar_data/mean(ar_data)
+  it_len <- length(ar_data_demean)
+  fl_atkinson <- 1 - sum(ar_data_demean^{fl_rho}*(1/it_len))^(1/fl_rho)
+  return(fl_atkinson)
+}
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+# Preference Vector 
+ar_rho <- c(1, 1 - (10^(c(seq(-2.2,2.2, length.out=60)))))
+ar_rho <- unique(ar_rho)
+mt_rho <- matrix(ar_rho, nrow=length(ar_rho), ncol=1)
+  
+# Random Data Vector (not equal outcomes)
+set.seed(123)
+ar_data_rand <- rnorm(15, mean=0,sd=1)
+ar_data_rand <- ar_data_rand - min(ar_data_rand) + 1
+
+# Uniform Data Vector (Equal)
+ar_data_unif <- rep(1, length(ar_data_rand))
+
+# One Rich (last person has income equal to the sum of all others*100)
+ar_data_onerich <- rep(0.1, length(ar_data_rand))
+ar_data_onerich[length(ar_data_onerich)] = sum(head(ar_data_onerich,-1))*10
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+# ATK = 0.1180513
+ffi_atkinson_ineq(ar_data_rand, -1)
+# ATK = 0
+ffi_atkinson_ineq(ar_data_unif, -1)
+# ATK = 0.89
+ffi_atkinson_ineq(ar_data_onerich, -1)
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+ar_rho
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+par(new=T)
+st_x_label <- 'Lambda, left Rawlsian, right (1) is Utilitarian'
+st_y_label <- 'Atkinson Inequality, 0 = perfect equal'
+ar_ylim = c(0,1)
+ffi_atkinson_ineq(ar_data_rand, -1)
+ar_atkinson <- apply(mt_rho, 1, function(row){ffi_atkinson_ineq(ar_data_rand, row[1])})
+plot(ar_rho, ar_atkinson, ylim = ar_ylim)
+title(main = 'A vector of Random data', xlab = st_x_label, ylab = st_y_label)
+grid()
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+par(new=T)
+ffi_atkinson_ineq(ar_data_onerich, -1)
+ar_atkinson <- apply(mt_rho, 1, function(row){ffi_atkinson_ineq(ar_data_onerich, row[1])})
+plot(ar_rho, ar_atkinson, ylim = ar_ylim)
+title(main = '1 person has the (income of all others summed up)*10', xlab = st_x_label, ylab = st_y_label)
+grid()
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+par(new=T)
+ffi_atkinson_ineq(ar_data_unif, -1)
+ar_atkinson <- apply(mt_rho, 1, function(row){ffi_atkinson_ineq(ar_data_unif, row[1])})
+plot(ar_rho, ar_atkinson, ylim = ar_ylim)
+title(main = 'uniform distribution', xlab = st_x_label, ylab = st_y_label)
+grid()
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
 # A as x-axis, need bounds on A
 fl_A_min = 0.01
 fl_A_max = 3
@@ -42,7 +114,7 @@ ls_df_indiff <- apply(tb_pref, 1, function(x){
 df_indiff <- do.call(rbind, ls_df_indiff) %>% drop_na()
 
 
-## ------------------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------------------------------------------------
 # Labeling
 st_title <- paste0('Indifference Curves Aktinson Atkinson Utility (CES)')
 st_subtitle <- paste0('Each Panel Different beta=A\'s Weight lambda=inequality aversion\n',
